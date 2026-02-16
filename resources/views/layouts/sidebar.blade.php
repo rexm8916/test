0cd2046d450d3eb1fce7f331cf7905552c6ad460
Mnifa8916 <mnifa8916tester@gmail.com>
2026-02-16 18:15:49 +0700

    add logo RizkiMandiri

--- resources/views/layouts/sidebar.blade.php
+++ resources/views/layouts/sidebar.blade.php
@@ -4,19 +4,19 @@
         <!-- Dark Logo-->
         <a href="{{ route('dashboard') }}" class="logo logo-dark">
             <span class="logo-sm">
-                <img src="{{ asset('assets/images/logo-sm.png') }}" alt="" height="22">
+                <span class="fs-22 fw-bold text-dark">RM</span>
             </span>
             <span class="logo-lg">
-                <img src="{{ asset('assets/images/logo-dark.png') }}" alt="" height="17">
+                <span class="fs-20 fw-bold text-dark">RizkiMandiri</span>
             </span>
         </a>
         <!-- Light Logo-->
         <a href="{{ route('dashboard') }}" class="logo logo-light">
             <span class="logo-sm">
-                <img src="{{ asset('assets/images/logo-sm.png') }}" alt="" height="22">
+                 <span class="fs-22 fw-bold text-white">RM</span>
             </span>
             <span class="logo-lg">
-                <img src="{{ asset('assets/images/logo-light.png') }}" alt="" height="17">
+                 <span class="fs-20 fw-bold text-white">RizkiMandiri</span>
             </span>
         </a>
         <button type="button" class="btn btn-sm p-0 fs-20 header-item float-end btn-vertical-sm-hover" id="vertical-hover">
