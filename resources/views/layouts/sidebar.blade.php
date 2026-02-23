020619899f854f2af5ea869333d2686af21d1fde
MamanHeryanto <maman.heryanto@indocyber.id>
2026-02-24 00:09:35 +0700

    fixing roles

--- resources/views/layouts/sidebar.blade.php
+++ resources/views/layouts/sidebar.blade.php
@@ -45,75 +45,75 @@
                 </li>
 
                 @if(auth()->check() && auth()->user()->isSuperAdmin())
-                <li class="menu-title"><span data-key="t-menu">Data Master</span></li>
-                <li class="nav-item">
-                    <a class="nav-link menu-link" href="{{ route('users.index') }}">
-                        <i class="ri-group-line"></i> <span data-key="t-users">Manajemen Pengguna</span>
-                    </a>
-                </li>
+                    <li class="menu-title"><span data-key="t-menu">Data Master</span></li>
+                    <li class="nav-item">
+                        <a class="nav-link menu-link" href="{{ route('users.index') }}">
+                            <i class="ri-group-line"></i> <span data-key="t-users">Manajemen Pengguna</span>
+                        </a>
+                    </li>
+                    
+                    <li class="nav-item">
+                        <a class="nav-link menu-link" href="{{ route('branches.index') }}">
+                            <i class="ri-store-2-line"></i> <span data-key="t-branches">Cabang Toko</span>
+                        </a>
+                    </li>
                 
-                <li class="nav-item">
-                    <a class="nav-link menu-link" href="{{ route('branches.index') }}">
-                        <i class="ri-store-2-line"></i> <span data-key="t-branches">Cabang Toko</span>
-                    </a>
-                </li>
-              
-                @endif
+                    {{-- coming soon --}}
+                    <li class="nav-item">
+                        <a class="nav-link menu-link" href="#sidebarDashboards" data-bs-toggle="collapse" role="button" aria-expanded="false" aria-controls="sidebarDashboards">
+                            <i class="ri-timer-line"></i> <span data-key="t-coming-soon">Coming Soon</span>
+                        </a>
+                        <div class="collapse menu-dropdown" id="sidebarDashboards">
+                            <ul class="nav nav-sm flex-column">
 
-                {{-- coming soon --}}
-                <li class="nav-item">
-                    <a class="nav-link menu-link" href="#sidebarDashboards" data-bs-toggle="collapse" role="button" aria-expanded="false" aria-controls="sidebarDashboards">
-                        <i class="ri-timer-line"></i> <span data-key="t-coming-soon">Coming Soon</span>
-                    </a>
-                    <div class="collapse menu-dropdown" id="sidebarDashboards">
-                        <ul class="nav nav-sm flex-column">
+                                <li class="menu-title"><span data-key="t-menu">Menu</span></li>
 
-                            <li class="menu-title"><span data-key="t-menu">Menu</span></li>
+                                <li class="nav-item">
+                                    <a class="nav-link" href="{{ route('dashboard') }}">
+                                        <i class="ri-dashboard-2-line"></i> <span data-key="t-dashboards">Dasbor</span>
+                                    </a>
+                                </li>
 
-                            <li class="nav-item">
-                                <a class="nav-link" href="{{ route('dashboard') }}">
-                                    <i class="ri-dashboard-2-line"></i> <span data-key="t-dashboards">Dasbor</span>
-                                </a>
-                            </li>
+                                <li class="menu-title"><i class="ri-more-fill"></i> <span data-key="t-pages">POS System</span></li>
 
-                            <li class="menu-title"><i class="ri-more-fill"></i> <span data-key="t-pages">POS System</span></li>
+                                <li class="nav-item">
+                                    <a class="nav-link" href="{{ route('transactions.index', ['type' => 'sale']) }}">
+                                        <i class="ri-exchange-dollar-line"></i> <span data-key="t-sales">Penjualan</span>
+                                    </a>
+                                </li>
 
-                            <li class="nav-item">
-                                <a class="nav-link" href="{{ route('transactions.index', ['type' => 'sale']) }}">
-                                    <i class="ri-exchange-dollar-line"></i> <span data-key="t-sales">Penjualan</span>
-                                </a>
-                            </li>
+                                <li class="nav-item">
+                                    <a class="nav-link" href="{{ route('debts.index') }}">
+                                        <i class="ri-book-mark-line"></i> <span data-key="t-debts">Hutang/Piutang</span>
+                                    </a>
+                                </li>
 
-                            <li class="nav-item">
-                                <a class="nav-link" href="{{ route('debts.index') }}">
-                                    <i class="ri-book-mark-line"></i> <span data-key="t-debts">Hutang/Piutang</span>
-                                </a>
-                            </li>
+                                <li class="nav-item">
+                                    <a class="nav-link" href="{{ route('customers.index') }}">
+                                        <i class="ri-user-line"></i> <span data-key="t-customers">Pelanggan</span>
+                                    </a>
+                                </li>
 
-                            <li class="nav-item">
-                                <a class="nav-link" href="{{ route('customers.index') }}">
-                                    <i class="ri-user-line"></i> <span data-key="t-customers">Pelanggan</span>
-                                </a>
-                            </li>
+                                <li class="menu-title"><i class="ri-more-fill"></i> <span data-key="t-pages">Management Barang</span></li>
 
-                            <li class="menu-title"><i class="ri-more-fill"></i> <span data-key="t-pages">Management Barang</span></li>
+                                <li class="nav-item">
+                                    <a class="nav-link" href="{{ route('transactions.index', ['type' => 'purchase']) }}">
+                                        <i class="ri-shopping-cart-line"></i> <span data-key="t-purchases">Pembelian</span>
+                                    </a>
+                                </li>
 
-                            <li class="nav-item">
-                                <a class="nav-link" href="{{ route('transactions.index', ['type' => 'purchase']) }}">
-                                    <i class="ri-shopping-cart-line"></i> <span data-key="t-purchases">Pembelian</span>
-                                </a>
-                            </li>
+                                <li class="nav-item">
+                                    <a class="nav-link" href="{{ route('products.index') }}">
+                                        <i class="ri-shopping-bag-3-line"></i> <span data-key="t-products">Produk</span>
+                                    </a>
+                                </li>
 
-                            <li class="nav-item">
-                                <a class="nav-link" href="{{ route('products.index') }}">
-                                    <i class="ri-shopping-bag-3-line"></i> <span data-key="t-products">Produk</span>
-                                </a>
-                            </li>
-
-                        </ul>
-                    </div>
-                </li>
-                {{-- end coming soon --}}
+                            </ul>
+                        </div>
+                    </li>
+                    {{-- end coming soon --}}
+                    
+                    @endif
             </ul>
         </div>
         <!-- Sidebar -->
